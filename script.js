be2eccfd02848d85ccbac73e5268dd6a060b190a
Alwin <81263845+alanjdjd@users.noreply.github.com>
2026-04-30 13:16:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function kontakt(){
+    console.log("kontakt")
+}
+function monolit(){
+    console.log("monolit")
+}
+function galeria(){
+    console.log("galeria")
+}
+
+window.addEventListener('load', () => {
+  const loader = document.getElementById('loader');
+  loader.classList.add('hidden');
+});
